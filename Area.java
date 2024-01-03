a379774d8f831e440fafa340a68499dd1bfb9dfd
imnikhilll <146981399+imnikhilll@users.noreply.github.com>
2024-01-03 23:18:15 +0530

    area of circle

---
+++ Area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+public class Area{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the radius of circle ");
+        float r=sc.nextFloat();
+        float Areaa=3.14f*r*r;
+        System.out.print(Areaa);
+
+    }
+}

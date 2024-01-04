5d9329cce59312879b470daa1007e1bf30db7d77
imnikhilll <146981399+imnikhilll@users.noreply.github.com>
2024-01-04 23:57:31 +0530

    input in java

---
+++ inputinjava.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class inputinjava{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the first number :");
+        int num1=sc.nextInt();
+        System.out.print("Enter the second number : ");
+        int num2=sc.nextInt();
+        System.out.print("Sum is : ");
+        int sum=num1+num2;
+        System.out.print(sum);
+
+        
+    }
+
+}
